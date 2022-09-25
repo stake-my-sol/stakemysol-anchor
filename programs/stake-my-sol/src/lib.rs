fd737de72bc358b53e591923f7d4eb592051d8b9
Vegan Cat <mmostafavi247@gmail.com>
2022-09-25 11:53:32 +0330

    Add stake account pubkey creation logic

--- programs/stake-my-sol/src/lib.rs
+++ programs/stake-my-sol/src/lib.rs
@@ -16,6 +16,7 @@ pub mod stake_my_sol {
         ctx: Context<CreateStakeAccountsAndDelegate>,
         total_stake_amount: u64,
         initial_seed_index: u32,
+        prefix_seed: String,
     ) -> Result<()> {
         let number_of_stake_accouns = ctx.remaining_accounts.len() as u64;
         let stake_account_amount = total_stake_amount / number_of_stake_accouns as u64;
@@ -25,16 +26,18 @@ pub mod stake_my_sol {
             // validate vote pubkeys
             require_keys_eq!(*vote_pubkey.owner, Vote::program::id());
 
-            // Todo: Create the stake pubkey
+            // Create the stake pubkey
             let stake_pubkey = Pubkey::create_with_seed(
-                &ctx.accounts.payer.key(),
-                &format!("stake-{}", initial_seed_index),
+                &ctx.accounts.staker.key(),
+                &format!("{}-{}", prefix_seed, seed_index),
                 ctx.program_id,
-            );
+            )
+            .unwrap();
 
-            // Todo: check if the stake account already exists
+            // Todo: check if the stake account has already been used
 
             // Todo: Create the stake account and Delegate
+
             // Todo: Stake::create_with_seed_and_delegate_stake
 
             seed_index += 1;
@@ -50,11 +53,25 @@ pub struct Initialize {}
 #[derive(Accounts)]
 pub struct CreateStakeAccountsAndDelegate<'info> {
     #[account(mut)]
-    payer: Signer<'info>,
+    staker: Signer<'info>,
     stake_authority: AccountInfo<'info>,
     withdraw_authority: AccountInfo<'info>,
     // *: Currently there is no anchor validation for Stake Program
     // *: We just check the program ID to make sure it's the Stake Program
     #[account(address = Stake::program::ID)]
     stake_program: AccountInfo<'info>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided vote account is not owned by the Vote Program")]
+    InvalidVoteAccount,
+    #[msg("The provided stake account is not owned by the Stake Program")]
+    InvalidStakeAccount,
+    #[msg("The provided stake account is already delegated")]
+    StakeAccountAlreadyDelegated,
+    #[msg("The provided stake account is already initialized")]
+    StakeAccountAlreadyInitialized,
+    #[msg("Failed to create a public key from seed")]
+    FailedToCreatePubkeyFromSeed,
 }
