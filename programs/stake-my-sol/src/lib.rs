20e3ee726aed207602a552d43a66cd30e8de4bb4
Vegan Cat <mmostafavi247@gmail.com>
2022-09-28 01:54:51 +0330

    Add staking logic

--- programs/stake-my-sol/src/lib.rs
+++ programs/stake-my-sol/src/lib.rs
@@ -8,10 +8,6 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod stake_my_sol {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-
     pub fn create_stake_accounts_and_delegate(
         ctx: Context<CreateStakeAccountsAndDelegate>,
         total_stake_amount: u64,
@@ -30,39 +26,55 @@ pub mod stake_my_sol {
         let number_of_stake_accouns = (remaining_accounts.len() as u64) / 2;
         let stake_amount_per_account = total_stake_amount / number_of_stake_accouns as u64;
         let mut seed_index = initial_seed_index;
+        let mut current_vote_account: &AccountInfo;
 
         for i in 0..number_of_stake_accouns as usize {
+            current_vote_account = &remaining_accounts[i];
+
             msg!("validating provided vote pubkeys");
             require_keys_eq!(*remaining_accounts[i].owner, Vote::program::id());
 
             msg!("Creating the stake pubkeys");
+            let current_seed = &format!("{}-{}", prefix_seed, seed_index);
+
             let stake_pubkey = Pubkey::create_with_seed(
                 &ctx.accounts.staker.key(),
-                &format!("{}-{}", prefix_seed, seed_index),
-                ctx.program_id,
+                current_seed,
+                &ctx.accounts.stake_program.key(),
             )
             .unwrap();
 
             msg!("Check if the respective input stake pubkey is equal to calculated one");
+            // if 0 <= i < number_of_stake_accouns then
+            // account info in index "i" would be a vote pubkey and
+            // its respective stake pubkey would be in
+            // "(i + number_of_stake_accouns)th" index in remaining accounts
             require_keys_eq!(
                 *remaining_accounts[i + (number_of_stake_accouns as usize)].owner,
                 stake_pubkey
             );
 
             // Todo: check if the stake account has already been used
-
             // Todo: Create the stake account and Delegate
-            // Stake::instruction::create_account_with_seed_and_delegate_stake(
-            //     // from_pubkey,
-            //     // stake_pubkey,
-            //     // base,
-            //     // seed,
-            //     // vote_pubkey,
-            //     // authorized,
-            //     // lockup,
-            //     // lamports
-            //     CpiContext::new(ctx.accounts.staker.to_account_info(), stake_pubkey),
-            // );
+            Stake::instruction::create_account_with_seed_and_delegate_stake(
+                &ctx.accounts.staker.key(),
+                &stake_pubkey,
+                &ctx.accounts.staker.key(),
+                current_seed,
+                &current_vote_account.key(),
+                &Stake::state::Authorized {
+                    staker: ctx.accounts.staker.key(),
+                    withdrawer: ctx.accounts.staker.key(),
+                },
+                &Stake::state::Lockup {
+                    unix_timestamp: 0,
+                    epoch: 0,
+                    custodian: ctx.accounts.staker.key(),
+                },
+                stake_amount_per_account,
+            );
+
+            // increment seed_index for next iteration
             seed_index += 1;
         }
 
