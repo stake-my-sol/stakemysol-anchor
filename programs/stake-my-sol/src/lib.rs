2b8aaee605648efdca6844b54e08c2e55e4b2c88
Vegan Cat <mmostafavi247@gmail.com>
2022-10-04 11:50:14 +0330

    Add logic for creating stake accounts

--- programs/stake-my-sol/src/lib.rs
+++ programs/stake-my-sol/src/lib.rs
@@ -1,20 +1,27 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::stake as Stake;
-use anchor_lang::solana_program::vote as Vote;
+use anchor_lang::solana_program::{
+    program::{invoke, invoke_signed},
+    stake as Stake, system_instruction, vote as Vote,
+};
 
-declare_id!("F5m8b7d8o3PMGyStiMdT6N6cttvMHWiXaJNWQ5Jve94k");
+declare_id!("CyUJ4YK5NoRopFsffbi4yKCNCScU9rCdtJ3dXmamfui1");
 
 #[program]
 pub mod stake_my_sol {
     use super::*;
 
-    pub fn create_stake_accounts_and_delegate(
-        ctx: Context<CreateStakeAccountsAndDelegate>,
+    pub fn create_stake_accounts_and_delegate<'info>(
+        ctx: Context<'_, '_, '_, 'info, CreateStakeAccountsAndDelegate<'info>>,
         total_stake_amount: u64,
-        initial_seed_index: u32,
+        initial_seed_index: u8,
         prefix_seed: String,
     ) -> Result<()> {
         let remaining_accounts = ctx.remaining_accounts;
+        let staker = &mut ctx.accounts.staker;
+        let stake_program = &ctx.accounts.stake_program;
+
+        msg!("validating passed stake program");
+        require_keys_eq!(stake_program.key(), Stake::program::id());
 
         msg!(
             "validating passed accounts to be even (i.e in pairs of vote pubkey and stake pubkey)"
@@ -23,13 +30,27 @@ pub mod stake_my_sol {
             return Err(ErrorCode::InvalidNumberOfAdditionalAccounts.into());
         }
 
-        let number_of_stake_accouns = (remaining_accounts.len() as u64) / 2;
+        let number_of_stake_accouns = (remaining_accounts.len()) / 2;
         let stake_amount_per_account = total_stake_amount / number_of_stake_accouns as u64;
-        let mut seed_index = initial_seed_index;
-        let mut current_vote_account: &AccountInfo;
 
-        for i in 0..number_of_stake_accouns as usize {
-            current_vote_account = &remaining_accounts[i];
+        let vote_pubkeys = remaining_accounts
+            .iter()
+            .take(number_of_stake_accouns)
+            .collect::<Vec<&AccountInfo>>();
+
+        let stake_pubkeys = remaining_accounts
+            .iter()
+            .skip(number_of_stake_accouns)
+            .collect::<Vec<&AccountInfo>>();
+        let mut seed_index = initial_seed_index;
+
+        for i in 0..number_of_stake_accouns {
+            let current_vote_account = vote_pubkeys[i];
+            // if 0 <= i < number_of_stake_accouns then
+            // account info in index "i" would be a vote pubkey and
+            // its respective stake pubkey would be in
+            // "(i + number_of_stake_accouns)th" index in remaining accounts
+            let repective_input_stake_pubkey = stake_pubkeys[i];
 
             msg!("validating provided vote pubkeys");
             require_keys_eq!(*remaining_accounts[i].owner, Vote::program::id());
@@ -37,44 +58,82 @@ pub mod stake_my_sol {
             msg!("Creating the stake pubkeys");
             let current_seed = &format!("{}-{}", prefix_seed, seed_index);
 
-            let stake_pubkey = Pubkey::create_with_seed(
-                &ctx.accounts.staker.key(),
-                current_seed,
-                &ctx.accounts.stake_program.key(),
-            )
-            .unwrap();
+            let calculated_stake_pubkey =
+                Pubkey::create_with_seed(&staker.key(), current_seed, &stake_program.key())
+                    .unwrap();
 
             msg!("Check if the respective input stake pubkey is equal to calculated one");
-            // if 0 <= i < number_of_stake_accouns then
-            // account info in index "i" would be a vote pubkey and
-            // its respective stake pubkey would be in
-            // "(i + number_of_stake_accouns)th" index in remaining accounts
-            require_keys_eq!(
-                *remaining_accounts[i + (number_of_stake_accouns as usize)].owner,
-                stake_pubkey
-            );
+            require_keys_eq!(repective_input_stake_pubkey.key(), calculated_stake_pubkey);
 
             // Todo: check if the stake account has already been used
-            msg!("Create stake account and delegate");
-            Stake::instruction::create_account_with_seed_and_delegate_stake(
-                &ctx.accounts.staker.key(),
-                &stake_pubkey,
-                &ctx.accounts.staker.key(),
-                current_seed,
-                &current_vote_account.key(),
-                &Stake::state::Authorized {
-                    staker: ctx.accounts.staker.key(),
-                    withdrawer: ctx.accounts.staker.key(),
-                },
-                &Stake::state::Lockup {
-                    unix_timestamp: 0,
-                    epoch: 0,
-                    custodian: ctx.accounts.staker.key(),
-                },
-                stake_amount_per_account,
-            );
 
-            // increment seed_index for next iteration
+            msg!("Creating the stake account");
+            invoke_signed(
+                &system_instruction::create_account_with_seed(
+                    &staker.key(),
+                    &repective_input_stake_pubkey.key(),
+                    &staker.key(),
+                    current_seed,
+                    stake_amount_per_account,
+                    std::mem::size_of::<Stake::state::StakeState>() as u64,
+                    &stake_program.key(),
+                ),
+                &[
+                    staker.to_account_info(),
+                    repective_input_stake_pubkey.to_account_info(),
+                    stake_program.to_account_info(),
+                ],
+                &[&[current_seed.as_bytes()]],
+            )?;
+
+            // *: An alternative implemetation
+            // system_program::create_account_with_seed(
+            //     CpiContext::new(
+            //         stake_program.to_account_info(),
+            //         system_program::CreateAccountWithSeed {
+            //             from: staker.to_account_info(),
+            //             to: repective_input_stake_pubkey.to_account_info(),
+            //             base: staker.to_account_info(),
+            //         },
+            //     ),
+            //     current_seed,
+            //     stake_amount_per_account,
+            //     std::mem::size_of::<Stake::state::StakeState>() as u64,
+            //     &stake_program.key(),
+            // )?;
+
+            msg!("Initializing the stake account");
+            invoke(
+                &Stake::instruction::initialize(
+                    &repective_input_stake_pubkey.key(),
+                    &Stake::state::Authorized {
+                        staker: staker.key(),
+                        withdrawer: staker.key(),
+                    },
+                    &Stake::state::Lockup {
+                        unix_timestamp: 0,
+                        epoch: 0,
+                        custodian: staker.key(),
+                    },
+                ),
+                &[repective_input_stake_pubkey.to_account_info()],
+            )?;
+
+            msg!("Delegating to current vote pubkey");
+            invoke(
+                &Stake::instruction::delegate_stake(
+                    &repective_input_stake_pubkey.key(),
+                    &staker.key(),
+                    &current_vote_account.key(),
+                ),
+                &[
+                    repective_input_stake_pubkey.to_account_info(),
+                    staker.to_account_info(),
+                    current_vote_account.to_account_info(),
+                    stake_program.to_account_info(), // !: not sure about this one!
+                ],
+            )?;
+
             seed_index += 1;
         }
 
@@ -82,17 +141,14 @@ pub mod stake_my_sol {
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
-
+// *: Currently there is no anchor validation for Stake Program
+// *: We just check the program ID to make sure it's the Stake Program
 #[derive(Accounts)]
 pub struct CreateStakeAccountsAndDelegate<'info> {
+    /// CHECK: There is no anchor native validation for the stake program
+    stake_program: AccountInfo<'info>,
     #[account(mut)]
     staker: Signer<'info>,
-    // *: Currently there is no anchor validation for Stake Program
-    // *: We just check the program ID to make sure it's the Stake Program
-    #[account(address = Stake::program::ID)]
-    stake_program: AccountInfo<'info>,
 }
 
 #[error_code]
--- tests/stake-my-sol.ts
+++ tests/stake-my-sol.ts
@@ -1,16 +1,59 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, BN } from "@project-serum/anchor";
+import { web3 } from "@project-serum/anchor";
 import { StakeMySol } from "../target/types/stake_my_sol";
+import {LAMPORTS_PER_SOL} from "@solana/web3.js"
+import { assert } from "chai"
+
+const stake_program = web3.StakeProgram
 
 describe("stake-my-sol", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
   const program = anchor.workspace.StakeMySol as Program<StakeMySol>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("create Stake Account And Split", async () => {
+    const seedPrefix = "slgjogjos";
+    const initialIndex = 0;
+    const totalStakeAmount = 2 * LAMPORTS_PER_SOL;
+    const voteAccount = new web3.PublicKey("9CdZxSmB6RH1Rcd4q2Wb56eFWDCu25TVs3484Y45W6rL");
+
+    const stakePubkey = await web3.PublicKey.createWithSeed(payer.publicKey, `${seedPrefix}-${initialIndex}`, stake_program.programId) 
+    console.log(stakePubkey.toBase58())
+
+    const tx = await program.methods.createStakeAccountsAndDelegate(
+      new BN(totalStakeAmount), 
+      0, 
+      seedPrefix,
+      )
+      .accounts({
+        staker: payer.publicKey,
+        stakeProgram: stake_program.programId,
+    }).remainingAccounts([{
+      pubkey: voteAccount, isSigner: false, isWritable: false
+    },{
+      pubkey: stakePubkey, isSigner: false, isWritable: false
+    }])
+    .signers([payer.payer])
+    .rpc();
+
+    const currentStakeAccounts = await provider
+      .connection
+      .getParsedProgramAccounts(stake_program.programId, {
+      filters: [
+        {
+          memcmp: {
+            offset: 12,
+            bytes: payer.publicKey.toBase58(),
+          },
+        },
+      ],
+    });
+
+    assert.equal(currentStakeAccounts.length, 1)
+
     console.log("Your transaction signature", tx);
-  });
+  })
 });
